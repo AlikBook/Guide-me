09bbaeef73f12c7a9a6a6b6588dc71fffb48a01d
AlikBook <ivanarturograndi6@gmail.com>
2025-06-12 12:23:06 +0200

    Created a simple home and menu linked with vue router

---
+++ front_end/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Metro app</h1>
+        <img class="ratp_metro" src="../../public/metrof_r.png" alt="">
+    </div>
+</template>
+
+<style>
+.ratp_metro{
+    width: 500px;    
+}
+</style>
