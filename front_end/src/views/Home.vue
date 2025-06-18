5eb4ce7640baaf63b1945c148811654a4c163023
Junior-Adjiey <koffi-jean-luc-junior.adjiey@efrei.net>
2025-06-18 22:00:40 +0200

    debut frontend

--- front_end/src/views/Home.vue
+++ front_end/src/views/Home.vue
@@ -1,32 +1,52 @@
 <template>
   <div>
     <h1>Metro app</h1>
-    <input type="number" name="Station 1" id="" v-model="station1">
-    <input type="number" name="Station 2" id="" v-model="station2">
-    <button @click="call_trip(station1,station2)">Test</button>
-    <p v-if="trip">Total time : {{ trip }}</p>
-   <ul v-if="trip && trip.stations">
-  <li v-for="lineObj in trip.stations" :key="Object.keys(lineObj)[0]">
-    <strong>{{ Object.keys(lineObj)[0] }}</strong>
-    <ul>
-      <li v-for="station in Object.values(lineObj)[0]" :key="station.id">
-        {{ station.station }}
-      </li>
-    </ul>
-  </li>
-</ul>
+    <input type="number" v-model="station1" placeholder="ID station départ">
+    <input type="number" v-model="station2" placeholder="ID station arrivée">
+    <button @click="call_trip(station1, station2)">Test</button>
 
-    <br>
+    <p v-if="trip && trip.total_time">Total time : {{ trip.total_time }}</p>
+
+    <!-- Affichage style Google Maps -->
+    <div v-if="trip && trip.stations" class="map-container">
+      <div
+        v-for="lineObj in trip.stations"
+        :key="Object.keys(lineObj)[0]"
+        class="line-section line-visual"
+        :class="getLineClass(Object.keys(lineObj)[0])"
+        :style="{ '--line-color' : getColorCode(Object.keys(lineObj)[0]) }"
+
+      >
+        <h3 class="line-name">{{ Object.keys(lineObj)[0] }}</h3>
+        <div class="line-track">
+          <div
+            v-for="(station, index) in Object.values(lineObj)[0]"
+            :key="station.id"
+            class="station-entry"
+          >
+            <div
+              class="circle"
+              :class="{
+                start: isFirstStation(lineObj, index),
+                end: isLastStation(lineObj, index)
+              }"
+            ></div>
+            <span class="station-label">{{ station.station }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br />
     <h1>All the station IDS</h1>
     <ul v-if="data && data.stations">
-        <li v-for="item in data.stations" :key="item.id">
-            ID: {{ item.id }} Station: {{ item.station }}
-        </li>
+      <li v-for="item in data.stations" :key="item.id">
+        ID: {{ item.id }} Station: {{ item.station }}
+      </li>
     </ul>
     <div v-else>Loading...</div>
-    <img class="ratp_metro" src="/metrof_r.png" alt="">
-    
-    
+
+    <img class="ratp_metro" src="/metrof_r.png" alt="Plan métro">
   </div>
 </template>
 
@@ -49,18 +69,109 @@ async function call_trip(value1, value2) {
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-      start: value1,
-      end: value2
-    })
-  });
-  const trip_data = await res.json();
-  trip.value = trip_data;
+    body: JSON.stringify({ start: value1, end: value2 })
+  })
+  trip.value = await res.json()
+}
+
+function getLineClass(lineName) {
+  return lineName.replace(/\s+/g, '-').toLowerCase()
+}
+
+function getColorCode(lineName) {
+  const normalized = lineName.trim().toLowerCase().replace(/\s+/g, '-')
+  const colors = {
+    'metro-1': '#FFD700',
+    'metro-2': '#0055C0',
+    'metro-3': '#9FC93C',
+    'metro-4': '#8B008B',
+    'metro-5': '#FF5C5C',
+    'metro-6': '#32CD32',
+    'metro-7': '#FF8C00',
+    'metro-8': '#800000',
+    'metro-9': '#E9967A',
+    'metro-10': '#4682B4',
+    'metro-11': '#DAA520',
+    'metro-12': '#556B2F',
+    'metro-13': '#1E90FF',
+    'metro-14': '#9400D3'
+  }
+  return colors[normalized] || '#000'
+}
+
+function isFirstStation(lineObj, index) {
+  return index === 0
+}
+
+function isLastStation(lineObj, index) {
+  return index === Object.values(lineObj)[0].length - 1
 }
 </script>
 
 <style>
-.ratp_metro{
-    width: 500px;    
+.ratp_metro {
+  width: 500px;
+  margin-top: 30px;
+}
+
+.map-container {
+  margin-top: 30px;
+}
+
+.line-section {
+  margin-bottom: 40px;
+}
+
+.line-name {
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.line-track {
+  position: relative;
+  padding-left: 30px;
+  margin-left: 20px;
+}
+
+.line-track::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 6px;
+  width: 4px;
+  background-color: var(--line-color);
+  border-radius: 2px;
+  z-index: 1;
+}
+
+
+.station-entry {
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin-bottom: 14px;
+}
+
+.circle {
+  width: 12px;
+  height: 12px;
+  border: 2px solid var(--line-color);
+  background-color: white;
+  border-radius: 50%;
+  position: absolute;
+  left: -28px;
+  top: 2px;
+  z-index: 2;
+}
+
+.circle.start,
+.circle.end {
+  background-color: var(--line-color);
+}
+
+.station-label {
+  font-size: 14px;
+  color: #111;
 }
 </style>
